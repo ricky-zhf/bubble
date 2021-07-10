4ddb6eaaf4e8ee1d9f713e34304cb64b611f0e8b
rickyy <568058082@qq.com>
2021-07-10 23:14:24 +0800

    init

--- main.go
+++ main.go
@@ -10,10 +10,18 @@ func main() {
 	gin.ForceConsoleColor()
 	gin.DefaultWriter = colorable.NewColorableStdout()
 	r := gin.Default()
+	//告诉gin去哪拉去模板文件引用的静态文件,即请求路径中/static是去static中去找。
+	r.Static("/static", "static")
+	//告诉gin去哪里找模板文件。
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-
+	// 待办事项
+	//添加待办
+	//查看待办
+	//更新带边
+	//删除待办
 	r.Run()
+
 }
